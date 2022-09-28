60ad15737d83fa6d191c9bb03536d4fd15ba5d65
adrian28s <addrians28@gmail.com>
2022-09-28 21:07:23 +0200

    funciones

---
+++ main.js
@@ -0,0 +1,73 @@
+// Crea un programa el cual te pregunte por una nota del 0 al 10 y dependiendo del número, te devuelva la siguiente clasificación.
+// Nota: Debes de usar el Switch.
+// 0 - 4: Insuficiente.
+// 5 - 6: Suficiente.
+// 7 - 8: Notable.
+// 9 - 10: Sobresaliente.
+// Crea una función resta que espere dos parámetros a y b y que devuelva la resta de los mismos
+
+// function miPorgrama() {
+//     let nota = prompt("Introduce tu nota");
+
+//     console.log(nota<=4)
+//     switch (true) {
+//         case nota <=4:
+//             console.log('insuficiente')
+//             break;
+
+//         case nota <=6:
+//             console.log("suficiente")
+//             break;
+
+//         case nota <=8:
+//             console.log("notable")
+//             break;
+
+//         case nota <=10:
+//             console.log("sobresaliente")
+//             break;
+
+//         default:5
+//             break;
+//     }
+
+// }
+
+// miPorgrama()
+
+//Crea una función resta que espere dos parámetros a y b y que devuelva la resta de los mismos.
+// function resta() {
+//     let number1 = prompt("introduce el 1º número")
+//     let number2 = prompt("introduce el 2º número")
+
+//     const resultado = number1 - number2
+//     console.log(resultado)    
+// }
+// resta()
+
+// Crea la función duplicaNumero debe recibir un tipo number y devolver el doble del valor recibido. Si la función no recibe un dato tipo number debe devolver el string ‘Debo ser ejecutada con un número’
+// function duplicaNumero() {
+//     const number1 = prompt("introduce tu número")
+//     const resultado = number1 * 2
+//     resultado = 
+//     console.log(resultado)
+//   return 
+    
+// }
+
+// Crea la función caracterInicial  debe recibir un tipo string y devolver un string con el primer carácter. 
+// Si la función no recibe un dato tipo string debe devolver el string 'Debo ser ejecutada con un string'. 
+// Si recibe un string vacío debe devolver 'Debo ser ejecutada con un string no vacío'
+
+// function caracterInicial(string) {
+// //     console.log(string)
+// //     if (typeof string != "string") {
+// //         return ("Debo ser ejecutada con un string");
+// //     }
+// //     else if (string === "") {
+// //         return ("Debo ser ejecutada con un string no vacío")
+// //     }
+// //     return string[0];
+
+// // }
+// // console.log(caracterInicial(""));
